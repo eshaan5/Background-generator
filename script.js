df0cf3de81ac69b2a5dd5d32a4a98af1f9bb9741
eshaan5 <eshaanbagga@gmail.com>
2022-04-07 21:12:23 +0530

    adding the files

---
+++ script.js
@@ -0,0 +1,20 @@
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
+var h3 = document.querySelector("h3")
+var body = document.querySelector("#gradient")
+
+function changeColor(){
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+}
+
+function displayColor(){
+    h3.innerHTML = ""
+    var color = body.style.background;
+    h3.textContent = color + ";";
+}
+
+color1.addEventListener("input", changeColor)
+color1.addEventListener("input", displayColor)
+
+color2.addEventListener("input", changeColor)
+color2.addEventListener("input", displayColor)
